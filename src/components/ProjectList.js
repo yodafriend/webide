ca556645baf52671ccd3fae763e8a86074457978
HYUNJOONKWAK <deartomefirst@gmail.com>
2023-10-11 18:07:30 +0900

    feat: create project[EBD-93]

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,11 +4,17 @@ import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import useAuthStore from '../auth/authStore';
+import CreateProjectModal from './CreateProject';
 
 function ProjectList() {
   const [projects, setProjects] = useState([]);
+  const [open, setOpen] = useState(false);
   const { token, logout } = useAuthStore();
   const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     axios
@@ -31,35 +37,44 @@ function ProjectList() {
   }, []);
 
   return (
-    <ul tw="divide-y divide-gray-100">
-      {projects.map((project) => (
-        <Link
-          to={`/ide/${project.projectId}`}
-          state={{ projectName: project.projectName }}
-          key={project.projectId}
-          tw="flex justify-center gap-x-7 py-5"
-        >
-          <div tw="flex min-w-0 gap-x-4">
-            <div tw="min-w-0 flex-auto">
-              <p tw="text-lg font-semibold leading-6 text-sky-900">
-                {project.projectName}
-              </p>
-              <p tw="mt-1 truncate text-base leading-5 text-sky-500">
-                {project.projectLang}
-              </p>
+    <>
+      <ul tw="divide-y divide-sky-100">
+        {projects.map((project) => (
+          <Link
+            to={`/ide/${project.projectId}`}
+            state={{ projectName: project.projectName }}
+            key={project.projectId}
+            tw="flex gap-x-7 py-5 justify-center"
+          >
+            <div tw="min-w-0 gap-x-4 border-solid rounded-lg border-2 border-sky-600 px-10 py-5">
+              <div tw="min-w-0 flex-auto">
+                <p tw="text-lg font-semibold leading-6 text-sky-900">
+                  {project.projectName}
+                </p>
+                <p tw="mt-1 truncate text-base leading-5 text-sky-500">
+                  {project.projectLang}
+                </p>
+                <p tw="mt-1 text-xs leading-5 text-sky-500">
+                  Created on{' '}
+                  <time dateTime={project.regDate}>{project.regDate}</time>
+                </p>
+              </div>
             </div>
-          </div>
-          <div tw="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p tw="text-sm leading-6 text-sky-900">{}</p>
+          </Link>
+        ))}
+        <div tw=" py-5">
+          <button
+            type="button"
+            onClick={() => setOpen(true)}
+            tw="min-w-0 w-48 border-solid rounded-lg border-2 px-10 py-5 min-w-0 text-lg font-semibold border-sky-600 text-sky-900 "
+          >
+            +
+          </button>
+        </div>
+      </ul>
 
-            <p tw="mt-1 text-xs leading-5 text-sky-500">
-              Created on{' '}
-              <time dateTime={project.regDate}>{project.regDate}</time>
-            </p>
-          </div>
-        </Link>
-      ))}
-    </ul>
+      <CreateProjectModal isOpen={open} onClose={handleCloseModal} />
+    </>
   );
 }
 
